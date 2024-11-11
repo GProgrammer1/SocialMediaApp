cc2a4969ece054dcd0a7d3c103f1fc6a5d10731f
Yahya-Akel <akelyahya89@gmail.com>
2024-11-11 21:21:01 +0200

    Settings and likes list design

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,FormsModule,RouterLink],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
+  styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
+  isNotificationsOn: boolean = false;
+  accessOptions: string[] = ['Public', 'Friends Only', 'Private'];
+  selectedAccessLevel: string = this.accessOptions[0];
 
+  toggleNotifications() {
+    this.isNotificationsOn = !this.isNotificationsOn;
+  }
+
+  // Additional helper method for conditional display
+  isSelectedOption(option: string): boolean {
+    return this.selectedAccessLevel === option;
+  }
 }
